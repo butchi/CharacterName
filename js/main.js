6aedb98a0a7bcd0379919efd18975b0354649495
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-03-23 22:57:34 +0900

    from SVG

---
+++ js/main.js
@@ -0,0 +1,55 @@
+(function (win, doc, $) {
+  var partsLi = [
+    'outline',
+    'eyes',
+    'hair',
+    'mouse',
+    'accessory'
+  ];
+
+  var elmLi = {};
+
+  function hideAll() {
+    // display to none
+    partsLi.forEach(function(part) {
+      elmLi[part].forEach(function(elm) {
+        $(elm).hide();
+      });
+    });
+  }
+
+  function init() {
+    $('#nameInput').on('keyup', function() {
+      update();
+    });
+
+    partsLi.forEach(function(part) {
+      elmLi[part] = [];
+      $('#' + part + ' > g').each(function(i, elm) {
+        elmLi[part].push(elm);
+      });
+    });
+
+    hideAll();
+    update()
+  }
+
+  function update() {
+    var str = $('#nameInput').text();
+    str = str.replace(/\s/g, '');
+
+    hideAll();
+
+    var i;
+    for(i = 0; i < 5; i++) {
+      var charCode = (str[i] || '' ).charCodeAt();
+      var partArr = elmLi[partsLi[i]];
+      var len = partArr.length;
+      var idx = charCode % len;
+
+      $(partArr[idx]).show();
+    }
+  }
+
+  $(init);
+}(window, document, jQuery));
